d9c2dd3b46d9184022f52630d5f4b1f35168c484
yuanxindong <yuanxin.me@gmail.com>
2019-11-23 16:04:05 +0800

    add genTimeoutErrorPromise

--- util.js
+++ util.js
@@ -1,3 +1,11 @@
 module.exports.genTimeoutPromise = function (timeout) {
   return new Promise(resolve => setTimeout(() => resolve(timeout), timeout))
 }
+
+module.exports.genTimeoutErrorPromise = function (timeout) {
+  return new Promise((resolve, reject) => setTimeout(
+      () => reject(new Error('Error timeout:', timeout)), 
+      timeout
+    )
+  )
+}
