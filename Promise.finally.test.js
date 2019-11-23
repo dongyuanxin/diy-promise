d83e8ba7fe29895e7aabcea4eff17ad85ff8ea10
yuanxindong <yuanxin.me@gmail.com>
2019-11-23 21:26:07 +0800

    add: error test for Promise.finally

--- Promise.finally.test.js
+++ Promise.finally.test.js
@@ -1,7 +1,14 @@
 const _ = require('./Promise.finally')
-const { genTimeoutPromise } = require('./util')
+const { genTimeoutPromise, genTimeoutErrorPromise } = require('./util')
 
 genTimeoutPromise(100)
+  .then(value => console.log('value is', value))
+  .catch(error => console.log('error is', error))
+  .finally(() => {
+    console.log('finally')
+  })
+
+genTimeoutErrorPromise(200)
   .then(value => console.log('value is', value))
   .catch(error => console.log('error is', error))
   .finally(() => {
