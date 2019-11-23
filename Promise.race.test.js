1aa8b934ca6d3fe73762dbcec9690cecd7e39ef9
yuanxindong <yuanxin.me@gmail.com>
2019-11-23 20:13:39 +0800

    change: replace Promise.race with Promise.myRace

--- Promise.race.test.js
+++ Promise.race.test.js
@@ -2,6 +2,6 @@ const _ = require('./Promise.race')
 const { genTimeoutPromise } = require('./util')
 
 const promises = [1, 2, 3].map(item => genTimeoutPromise(item * 100))
-Promise.race(promises)
+Promise.myRace(promises)
   .then(value => console.log(value))
   .catch(error => console.log('error is', error))
